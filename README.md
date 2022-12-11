4c7737f55514738225854c465b76c3a52d02eb03
TakeAsh <take@TakeAsh.net>
2022-12-11 19:03:29 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Data::HTML::TreeDumper - It's new $module
+
+# SYNOPSIS
+
+    use Data::HTML::TreeDumper;
+
+# DESCRIPTION
+
+Data::HTML::TreeDumper is ...
+
+# LICENSE
+
+Copyright (C) TakeAsh.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+TakeAsh &lt;take@TakeAsh.net>
---
+++ lib/Data/HTML/TreeDumper.pm
@@ -0,0 +1,39 @@
+package Data::HTML::TreeDumper;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::HTML::TreeDumper - It's new $module
+
+=head1 SYNOPSIS
+
+    use Data::HTML::TreeDumper;
+
+=head1 DESCRIPTION
+
+Data::HTML::TreeDumper is ...
+
+=head1 LICENSE
+
+Copyright (C) TakeAsh.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+TakeAsh E<lt>take@TakeAsh.netE<gt>
+
+=cut
+
